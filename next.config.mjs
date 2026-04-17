6a5a8527f576c8eaef685a8a1b5777154cbd3274
havinfun47 <graydon@scalescientist.com>
2026-04-16 19:02:29 -0700

    Remove basePath for custom domain scalescientist.com

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/ss-main-website",
   images: { unoptimized: true },
   env: {
-    NEXT_PUBLIC_BASE_PATH: "/ss-main-website",
+    NEXT_PUBLIC_BASE_PATH: "",
   },
 };
 
